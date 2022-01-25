7edb3b2dbc4219546cfd8ee5fb1fbebfe50e229f
Bibin Abraham Alex <97511694+Bibin1999@users.noreply.github.com>
2022-01-25 08:51:00 +0530

    Add files via upload

---
+++ Assignment 6/StudentName.java
@@ -0,0 +1,11 @@
+class StudentName
+{
+int rollno=10;
+String name=("Bibin Abraham");
+public static void main(String[] args)
+{
+Student stud=new Student();
+System.out.println(stud.name);
+System.out.println(stud.rollno);
+}
+}
