9ae637106f4aba2a679334d476d664ecbaa4c52d
Bibin Abraham Alex <97511694+Bibin1999@users.noreply.github.com>
2022-01-25 08:51:52 +0530

    Add files via upload

---
+++ Assignment 7/MailID.java
@@ -0,0 +1,13 @@
+package JavaProgram1;
+
+public class MailID 
+{
+public static void main(String[] args) 
+{
+	String a1="bibinalex";
+	String a2="1999@";
+	String a3="gmail.com";
+	String a4=a1.concat(a2).concat(a3);
+	System.out.println(a4);
+}
+}
